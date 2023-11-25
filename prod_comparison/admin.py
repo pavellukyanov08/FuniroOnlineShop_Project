3d9d110687f99bea36f169bc58a1b42eb0a93b8b
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-11-25 15:52:46 +0700

    Update admin panel

--- prod_comparison/admin.py
+++ prod_comparison/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Compare
 
 
-admin.site.register(Compare)
+class CompareAdmin(admin.ModelAdmin):
+    list_display = ['product', 'user']
+    list_filter = ['product', 'user']
+    ordering = ('product',)
+
+
+admin.site.register(Compare, CompareAdmin)
--- shopping_cart/admin.py
+++ shopping_cart/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import ShoppingCart
 
-admin.site.register(ShoppingCart)
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'show_image', 'quantity', 'user']
+    list_filter = ['name', 'price', 'user']
+    ordering = ('product',)
+
+    def show_image(self, obj):
+        if obj.img:
+            return mark_safe(f"<img src='{obj.img.url}' width='50' height='50' />")
+        return 'None'
+
+    show_image.__name__ = 'Картинка'
