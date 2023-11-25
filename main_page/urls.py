6d27b601cbc0636f54293eb57a19129598fa585b
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-11-25 17:50:22 +0700

    Fixed bug with deleting product

--- main_page/urls.py
+++ main_page/urls.py
@@ -7,14 +7,14 @@ urlpatterns = [
     # main_page
     path('', views.products_list, name='index'),
 
+    # delete prod
+    path('product/<int:prod_id>/delete', views.delete_prod, name='delete'),
+
     # adding product
     path('add_product/', views.add_product, name='add_product'),
 
     # edit product page
     path('product/<int:prod_id>/<slug:slug>', views.product_detail, name='product_detail'),
-
-    # delete prod
-    path('product/<int:prod_id>/delete', views.delete_prod, name='delete'),
 
     # product category
     path('category/<slug:category_slug>/', views.products_list, name='product_list_by_category'),
