d7848cfe5e1768fa85343a5d6e13cae8bc7173b5
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-11-25 19:46:40 +0700

    Change urls file

--- main_page/urls.py
+++ main_page/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.products_list, name='index'),
 
     # delete prod
-    path('product/<int:prod_id>/delete', views.delete_prod, name='delete'),
+    path('product/ <int:prod_id>/delete', views.delete_prod, name='delete'),
 
     # adding product
     path('add_product/', views.add_product, name='add_product'),
